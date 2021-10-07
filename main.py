28e416ed6bb259b9f924cb7eae7497477c37a8aa
sahare92 <sahar@LAPTOP-5K5P96DB.localdomain>
2021-10-07 12:22:16 +0300

    updated code

--- main.py
+++ main.py
@@ -6,24 +6,35 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-USERS = {
-    'sahar': {},
-    'liav': {},
-    'lhabin': {},
+USER_TO_PASSWORD = {
+    'sahar': '123456',
+    'liav': '123',
+    'lhabin': '422542',
 }
 
 
 class Login(Resource):
-    def get(self):
-        return {}
+    # def get(self):
+    #     return {}
 
     def post(self):
-        # TODO: implement this part (add login validation here)
+        """
+        Post will receive user credentials, and will return True response if they're ok
+        Input: {'username': <username>, 'password': <password>}
+        """
         response = {}
-
-        request_data = request.get_data()
-
+        request_data = json.loads(request.get_data())
         print(str(request_data))
+
+        # TODO: implement this part (add login validation here)
+        request_username = request_data['username']
+        request_password = request_data['password']
+
+        if USER_TO_PASSWORD[request_username] == request_password:
+            response = {'response': 'ok'}
+        else:
+            response = {'response': 'unauthorized'}
+        # TODO: END of your code
 
         return response
 
@@ -33,3 +44,4 @@ api.add_resource(Login, '/login')
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
+
